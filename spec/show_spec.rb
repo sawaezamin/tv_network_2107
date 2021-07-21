3eff03dc10c9701e8021d3274c98e27b849061b2
Sawaez Amin <sawaezamin@gmail.com>
2021-07-21 09:30:04 -0600

    Work on iteration 2

--- spec/show_spec.rb
+++ spec/show_spec.rb
@@ -3,17 +3,16 @@ require './lib/show'
 
 RSpec.describe Show do
   before :each do
-    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
-
     @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
     @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
   end
 
-  xit 'exists' do
+  it 'exists' do
     expect(@knight_rider).to be_a(Show)
   end
 
-  xit 'has attribues' do
+  it 'has attribues' do
     expect(@knight_rider.name).to eq("Knight Rider")
     expect(@knight_rider.creator).to eq("Glen Larson")
     expect(@knight_rider.characters).to eq([@michael_knight, @kitt])
